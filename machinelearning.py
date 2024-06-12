2e736736a588dfca982d19a1b8a47d37d16e90c6
SafinaOz <s.m.o.ozkeles@student.tue.nl>
2024-06-12 10:18:45 +0200

    kwaliteit van classifiers verbeterd en gesplit

--- machinelearning.py
+++ machinelearning.py
@@ -15,7 +15,7 @@ import math
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 from sklearn.multioutput import MultiOutputClassifier
 def read_data():
     data_raw = pd.read_csv('tested_molecules.csv')
@@ -37,24 +37,41 @@ def machine_learning():
     y = data_raw[['PKM2_inhibition','ERK2_inhibition']]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
-    rfc = MultiOutputClassifier(RandomForestClassifier(n_estimators=20, random_state=42, class_weight='balanced'))
-    knn = MultiOutputClassifier(KNeighborsClassifier(n_neighbors=int(math.sqrt(len(df_molecules['mol'])))))
+    rfc = MultiOutputClassifier(RandomForestClassifier(n_estimators=20, random_state=42, class_weight='balanced')) #n_estimators kan anders
+    knn = MultiOutputClassifier(KNeighborsClassifier(n_neighbors=int(math.sqrt(len(df_molecules['mol']))))) #n_neighbors kan anders
     return rfc, knn, X_test, y_test, X_train, y_train
 
-def predict(clf, X_test, y_test, X_train, y_train):    
+def predict(clf, X_test, y_test, X_train, y_train):
+    # we moeten alles los bekijken, want dan krijg je veel beter inzicht.    
     clf.fit(X_train, y_train)
+    y_true = y_test
     y_pred = clf.predict(X_test)
+    y_pred_df = pd.DataFrame(y_pred, columns =['PKM2_inhibition','ERK2_inhibition'])
+    #confusion matrix berekenen
+    cm_PKM2 = confusion_matrix(y_true['PKM2_inhibition'],y_pred_df['PKM2_inhibition'])
+    tn_PKM2, fp_PKM2, fn_PKM2, tp_PKM2 = cm_PKM2.ravel()
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average = 'macro', zero_division=1)
-    recall = recall_score(y_test, y_pred, average = 'macro')
-    f1 = f1_score(y_test, y_pred,average = 'macro' )
-    roc_auc = roc_auc_score(y_test, y_pred, average = 'macro')
-    print('Accuracy: ', accuracy)
-    print('Precision: ', precision)
-    print('Recall: ', recall)
+    cm_ERK2 = confusion_matrix(y_true['ERK2_inhibition'],y_pred_df['ERK2_inhibition'])
+    tn_ERK2, fp_ERK2, fn_ERK2, tp_ERK2 = cm_ERK2.ravel()
+    #accuracy berekenen
+    accuracy_PKM2 = accuracy_score(y_true['PKM2_inhibition'], y_pred_df['PKM2_inhibition'])
+    accuracy_ERK2 = accuracy_score(y_true['ERK2_inhibition'], y_pred_df['ERK2_inhibition'])
+    #precision berekenen
+    precision_PKM2 = precision_score(y_true['PKM2_inhibition'], y_pred_df['PKM2_inhibition'], zero_division='warn')
+    precision_ERK2 = precision_score(y_true['ERK2_inhibition'], y_pred_df['ERK2_inhibition'], zero_division='warn')
+    #sensitivity berekenen
+    sensitivity_PMK2 = recall_score(y_true['PKM2_inhibition'], y_pred_df['PKM2_inhibition'])
+    sensitivity_ERK2 = recall_score(y_true['ERK2_inhibition'], y_pred_df['ERK2_inhibition'])
+    f1 = f1_score(y_true, y_pred,average = 'macro' )
+    roc_auc = roc_auc_score(y_true, y_pred, average = 'macro')
+    #tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+
+    print('Accuracy: ', accuracy_PKM2, accuracy_ERK2)
+    print('Precision: ', precision_PKM2, precision_ERK2)
+    print('Sensitivity: ', sensitivity_PMK2, sensitivity_ERK2)
     print('F1: ', f1)
     print('ROC AUC: ', roc_auc)
+    #print(tn)
 
     return y_pred
 
